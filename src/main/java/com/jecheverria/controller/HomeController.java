504c25efe2b567b1223360044711a682e8170202
Jeison Echeverria <jeisonecheverria22@gmail.com>
2022-05-01 13:11:11 -0500

    se agrego el primer modelo y se agregaron vacantes de empleo

--- src/main/java/com/jecheverria/controller/HomeController.java
+++ src/main/java/com/jecheverria/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.jecheverria.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
 	public String mostrarHome() {
-		return"home";
+		return "home";
+	}
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		List<String> lista = new LinkedList<String>();
+		lista.add("Ingeniero de sistemas");
+		lista.add("Auxiliar de contabilidad");
+		lista.add("Vendedor");
+		lista.add("Arquitecto");
+		model.addAttribute("empleos", lista);
+		return "listado";
 	}
 
 }
