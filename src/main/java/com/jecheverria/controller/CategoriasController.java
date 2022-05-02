491ecac7385103ec1d839c8f0526a00c4c152a18
Jeison Echeverria <jeisonecheverria22@gmail.com>
2022-05-02 10:47:49 -0500

    Se agrego un nuevo controlador para las categorias y se crearon las vistas que devuelve

--- src/main/java/com/jecheverria/controller/CategoriasController.java
+++ src/main/java/com/jecheverria/controller/CategoriasController.java
@@ -1,5 +1,27 @@
 package com.jecheverria.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categorias")
 public class CategoriasController {
+	@GetMapping("/index")
+	public String mostrarIndex(Model model) {
+		return "categorias/listaCategorias";
+	}
+
+	@GetMapping("/crear")
+	public String crear() {
+		return "categorias/formularioCategorias";
+	}
+
+	@PostMapping("/guardar")
+	public String guardar() {
+		return "categorias/listaCategorias";
+	}
 
 }
